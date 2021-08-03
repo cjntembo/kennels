8717b1358b73e65f054cf3306078ec29a8d04da6
Candace Ntembo <cjntembo@gmail.com>
2021-08-02 21:02:46 -0500

    first commit

---
+++ src/components/locations/Locations.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Locations.css"
+
+export const Location = () => (
+    <section className="location">
+        <h3 className="location__name">Nashville Kennels North</h3>
+        <div className="location__branch">200 Main Street</div>
+    </section>
+)
